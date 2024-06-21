0d5e773f915a6623115529883a9e05a646e3435f
Dylan Roelfs <43468174+dylanroelfs@users.noreply.github.com>
2024-06-21 15:46:36 +0200

    Update config.php
    DB Password was visible.

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ error_reporting(E_All);
 ini_set('display_errors', '1');
 
 $db_hostname = 'localhost';
-$db_username = 'u72381p270346_admin';
-$db_password = 'jykfUv-tecse1-dozfiq';
-$db_database =  'u72381p270346_DB';
+$db_username = '#';
+$db_password = '#';
+$db_database = '#';
 
 $mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
 
